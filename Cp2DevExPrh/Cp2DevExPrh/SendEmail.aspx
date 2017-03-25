3cfcfdbdcfbb7d96d7af5f666980f3b05434b92e
mstftikir <tikir@itu.edu.tr>
2017-03-25 14:00:11 +0200

    Email

---
+++ Cp2DevExPrh/Cp2DevExPrh/SendEmail.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SendEmail.aspx.cs" Inherits="Cp2DevExPrh.SendEmail" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <p>E-postalar gönderildi.</p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Cp2DevExPrh/Cp2DevExPrh/SendEmail.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cp2DevExPrh
+{
+    public partial class SendEmail : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.webmail.com");
+
+                mail.From = new MailAddress("tikir@itu.edu.tr");
+                mail.To.Add("tikir@itu.edu.tr");
+                mail.Subject = "Test Mail";
+                mail.Body = "This is for testing SMTP mail from GMAIL";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("username", "password");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                Response.Write("Succesfull.");
+          }
+            catch (Exception ex)
+            {
+                Response.Write("Could not send the e-mail - error: " + ex.Message);
+            }
+             
+        }
+    }
+}
