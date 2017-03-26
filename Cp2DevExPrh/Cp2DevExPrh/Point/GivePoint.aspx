3391162161a9d0d550347483c5951c335ee6df3e
ZİHNİCANBEGBURS <begburs@itu.edu.tr>
2017-03-26 21:11:29 +0300

    point operations is added

---
+++ Cp2DevExPrh/Cp2DevExPrh/Point/GivePoint.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Main.master" CodeBehind="GivePoint.aspx.cs" Inherits="Cp2DevExPrh.Point.GivePoint" %>
+
+
+
+<asp:Content ID="Content" ContentPlaceHolderID="MainContent" runat="server">
+
+    
+        
+    
+    <dx:ASPxButton ID="Button1" runat="server" Text="Excel Indir" OnClick="Button_Download" />
+    <h4><asp:Literal ID="ltConnecitonMessage" runat="server" /></h4>
+    <asp:GridView ID="GridView1" runat="server" ></asp:GridView>
+    <asp:GridView ID="GridView2"  runat="server"  ></asp:GridView>
+    <asp:FileUpload ID="FileUpload2" runat="server" />
+    <dx:ASPxButton ID="Button2" runat="server" Text="Excel Yukle" OnClick="Button_Import" />
+
+</asp:Content>
---
+++ Cp2DevExPrh/Cp2DevExPrh/Point/GivePoint.aspx.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using QC = System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System.Configuration;
+using System.Data.OleDb;
+using Microsoft.Office;
+using ClosedXML.Excel;
+
+namespace Cp2DevExPrh.Point
+{
+    public partial class GivePoint : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadGrid();
+
+            }
+
+        }
+
+        protected void Button_Download(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable("Puanlama");
+            foreach (TableCell cell in GridView1.HeaderRow.Cells)
+            {
+                dt.Columns.Add(cell.Text);
+            }
+            foreach (GridViewRow row in GridView1.Rows)
+            {
+                dt.Rows.Add();
+                for (int i = 0; i < row.Cells.Count; i++)
+                {
+                    dt.Rows[dt.Rows.Count - 1][i] = row.Cells[i].Text;
+                }
+            }
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                wb.Worksheets.Add(dt);
+
+                Response.Clear();
+                Response.Buffer = true;
+                Response.Charset = "";
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("content-disposition", "attachment;filename=Puanlama.xlsx");
+
+                using (MemoryStream MyMemoryStream = new MemoryStream())
+                {
+                    wb.SaveAs(MyMemoryStream);
+                    MyMemoryStream.WriteTo(Response.OutputStream);
+                    Response.Flush();
+                    Response.End();
+                }
+            }
+
+        }
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+            /*Tell the compiler that the control is rendered
+             * explicitly by overriding the VerifyRenderingInServerForm event.*/
+        }
+
+        protected void LoadGrid()
+        {
+            SqlConnection dbConnection = new SqlConnection(@"Data Source=babalarserver.database.windows.net;Database=hasbabalarDB;Integrated Security=False;User ID=babalar;Password=Gelburayi123;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            dbConnection.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter("Select Kullanici.KullaniciAdi,Kullanici.KullaniciMaili,Restoran.Ad,Puanlama.Puan from Puanlama INNER JOIN  Kullanici ON Kullanici.KullaniciID = Puanlama.KullaniciID INNER JOIN  Restoran ON Restoran.RestoranID = Puanlama.RestoranID", dbConnection);
+
+            DataTable ds = new DataTable();
+
+            da.Fill(ds);
+
+            GridView1.DataSource = ds;
+
+            GridView1.DataBind();
+            dbConnection.Close();
+        }
+
+
+        protected void Button_Import(object sender, EventArgs e)
+        {
+            if (FileUpload2.HasFile)
+            {
+                string path = string.Concat(Server.MapPath("~/File/" + FileUpload2.FileName));
+                FileUpload2.SaveAs(path);
+
+                OleDbConnection OleDbcon;
+                OleDbCommand cmd = new OleDbCommand(); ;
+                OleDbDataAdapter objAdapter1 = new OleDbDataAdapter();
+                DataSet ds = new DataSet();
+                DataTable dtExcelData = new DataTable();
+
+                if (Path.GetExtension(path) == ".xls")
+                {
+                    OleDbcon = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path + "; Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=2\"");
+                    OleDbcon.Open();
+                    cmd.Connection = OleDbcon;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT * FROM [Puanlama$]";
+                    objAdapter1 = new OleDbDataAdapter(cmd);
+                    objAdapter1.Fill(ds);
+                    dtExcelData = ds.Tables[0];
+                    GridView2.DataSource = dtExcelData;
+                    GridView2.DataBind();
+                    OleDbcon.Close();
+                }
+                else if (Path.GetExtension(path) == ".xlsx")
+                {
+                    OleDbcon = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + "; Extended Properties='Excel 12.0;HDR=YES;IMEX=1;';");
+                    OleDbcon.Open();
+                    cmd.Connection = OleDbcon;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT * FROM [Puanlama$]";
+                    objAdapter1 = new OleDbDataAdapter(cmd);
+                    objAdapter1.Fill(ds);
+                    dtExcelData = ds.Tables[0];
+                    GridView2.DataSource = dtExcelData;
+
+                    GridView2.DataBind();
+                    OleDbcon.Close();
+                }
+
+
+
+            }
+
+
+
+
+            SqlConnection dbConnection = new SqlConnection(@"Data Source=babalarserver.database.windows.net;Database=hasbabalarDB;Integrated Security=False;User ID=babalar;Password=Gelburayi123;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            dbConnection.Open();
+            foreach (GridViewRow row in GridView2.Rows)
+            {
+                if (row.RowType == DataControlRowType.DataRow)
+                {
+                    String KullaniciAdi = row.Cells[0].Text;
+                    String KullaniciMaili = row.Cells[1].Text;
+                    String RestoranAdi = row.Cells[2].Text;
+                    int Puan = Int32.Parse(row.Cells[3].Text);
+
+                    SqlCommand cmdd = new SqlCommand(" Update Puanlama SET Puan ='" + Puan + "' WHERE  KullaniciID IN (SELECT KullaniciID  FROM Kullanici WHERE KullaniciAdi = '" + KullaniciAdi + "'  AND  KullaniciMaili ='" + KullaniciMaili + "' ) AND RestoranID IN (SELECT RestoranID  FROM Restoran WHERE  Ad = '" + RestoranAdi + "')", dbConnection);
+                    cmdd.ExecuteNonQuery();
+
+
+                }
+            }
+
+            dbConnection.Close();
+            LoadGrid();
+        }
+
+
+    }
+}
